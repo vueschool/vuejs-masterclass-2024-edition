7c562da7a441bb6e2c01cd135f3dd56ac271e3b1
Mostafa Said <mostafasaid1994@gmail.com>
2024-05-24 19:17:42 +0300

    feat: :sparkles: refactor codebase to support file-based routing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,16 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
-
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
   },
-
+  rules: {
+    'vue/multi-word-component-names': 0
+  }
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,6 @@
 <template>
   <div>
     <h1>Home Page</h1>
-    <RouterLink to="/projects">Go to projects</RouterLink>
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink>
   </div>
 </template>
