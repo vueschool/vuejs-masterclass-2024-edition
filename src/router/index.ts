354efae17d881827bfb1ccdb19131fd353992e01
Mostafa Said <mostafasaid1994@gmail.com>
2024-05-24 14:41:24 +0300

    feat: :sparkles: setup unplugin-vue-router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import { h } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router/auto'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue')
-    },
-    {
-      // /projects/dfioahiugfdbsiufa
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue')
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'NotFound',
-      component: h('p', { style: 'color: red;' }, '404 Project Not Found')
-    }
-  ]
+  history: createWebHistory(import.meta.env.BASE_URL)
 })
 
 export default router
