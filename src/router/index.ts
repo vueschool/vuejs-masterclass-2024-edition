78876d20cf51fa63cc28a89b6c91cf8654c0c01c
Mostafa Said <mostafasaid1994@gmail.com>
2024-05-29 16:40:13 +0300

    fix: :bug: upgrade unplugin-vue-router version and apply breaking changes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL)
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
 })
 
 export default router
