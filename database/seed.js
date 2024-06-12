ae1edc60ddbb986faa7c8302947a4f653581fdee
Mostafa Said <mostafasaid1994@gmail.com>
2024-06-13 01:14:38 +0300

    feat: :sparkles: add supabase to seed.js

--- database/seed.js
+++ database/seed.js
@@ -1,6 +1,16 @@
+/* eslint-env node */
+
 import { fakerEN_US as faker } from '@faker-js/faker'
+import { createClient } from '@supabase/supabase-js'
+// In case you're using Node version older than v20
+// Install dotenvx by running the following command npm install @dotenvx/dotenvx --save
+// Next, add the following code to the file
+//
+// import { config } from '@dotenvx/dotenvx'
+// config()
+//
+// With that done, you can now use env variables as usual
 
-const personName = faker.person.fullName()
-const personBio = faker.person.bio()
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-console.log('Hi! My name is ', personName, '. I am a ', personBio)
+console.log(supabase)
