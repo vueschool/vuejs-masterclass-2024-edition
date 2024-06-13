25db674c19b07eb93528593145f094f3a7ff30af
Mostafa Said <mostafasaid1994@gmail.com>
2024-06-14 00:16:48 +0300

    feat: :sparkles: seeding projects table

--- database/seed.js
+++ database/seed.js
@@ -2,15 +2,24 @@
 
 import { fakerEN_US as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
-// In case you're using Node version older than v20
-// Install dotenvx by running the following command npm install @dotenvx/dotenvx --save
-// Next, add the following code to the file
-//
-// import { config } from '@dotenvx/dotenvx'
-// config()
-//
-// With that done, you can now use env variables as usual
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-console.log(supabase)
+const seedProjects = async (numEntries) => {
+  const projects = []
+
+  for (let i = 0; i < numEntries; i++) {
+    const name = faker.lorem.words(3)
+
+    projects.push({
+      name: name,
+      slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: faker.helpers.arrayElements([1, 2, 3])
+    })
+  }
+
+  await supabase.from('projects').insert(projects)
+}
+
+await seedProjects(10)
