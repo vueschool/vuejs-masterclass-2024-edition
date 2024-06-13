f3aa8d70d675782b6438cc1b53a8ca3e36ba962d
Mostafa Said <mostafasaid1994@gmail.com>
2024-06-14 00:41:46 +0300

    feat: :sparkles: refactor seed.js

--- database/seed.js
+++ database/seed.js
@@ -5,7 +5,19 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
+const logErrorAndExit = (tableName, error) => {
+  console.error(
+    `An error occurred in table '${tableName}' with code ${error.code}: ${error.message}`
+  )
+  process.exit(1)
+}
+
+const logStep = (stepMessage) => {
+  console.log(stepMessage)
+}
+
 const seedProjects = async (numEntries) => {
+  logStep('Seeding projects...')
   const projects = []
 
   for (let i = 0; i < numEntries; i++) {
@@ -19,7 +31,19 @@ const seedProjects = async (numEntries) => {
     })
   }
 
-  await supabase.from('projects').insert(projects)
+  const { data, error } = await supabase.from('projects').insert(projects).select('id')
+
+  if (error) return logErrorAndExit('Projects', error)
+
+  logStep('Projects seeded successfully.')
+
+  return data
 }
 
-await seedProjects(10)
+const seedDatabase = async (numEntriesPerTable) => {
+  await seedProjects(numEntriesPerTable)
+}
+
+const numEntriesPerTable = 10
+
+seedDatabase(numEntriesPerTable)
