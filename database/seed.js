041fc675c5b4ce138529424de9e89b0f440ff09b
Mostafa Said <mostafasaid1994@gmail.com>
2024-06-14 01:26:59 +0300

    fix: :bug: update lesson code

--- database/seed.js
+++ database/seed.js
@@ -5,21 +5,15 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-const seedProjects = async (numEntries) => {
-  const projects = []
+const seedProjects = async () => {
+  const name = faker.lorem.words(3)
 
-  for (let i = 0; i < numEntries; i++) {
-    const name = faker.lorem.words(3)
-
-    projects.push({
-      name: name,
-      slug: name.toLocaleLowerCase().replace(/ /g, '-'),
-      status: faker.helpers.arrayElement(['in-progress', 'completed']),
-      collaborators: faker.helpers.arrayElements([1, 2, 3])
-    })
-  }
-
-  await supabase.from('projects').insert(projects)
+  await supabase.from('projects').insert({
+    name: name,
+    slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+    status: faker.helpers.arrayElement(['in-progress', 'completed']),
+    collaborators: faker.helpers.arrayElements([1, 2, 3])
+  })
 }
 
-await seedProjects(10)
+await seedProjects()
