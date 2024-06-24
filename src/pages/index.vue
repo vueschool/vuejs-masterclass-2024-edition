2fc9603ad423e8e3a5ce3911ec031364925c225b
Mostafa Said <mostafasaid1994@gmail.com>
2024-06-24 10:04:35 +0300

    feat: :sparkles: using refs to store projects

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,4 @@
-<script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-;(async () => {
-  const { data, error } = await supabase.from('projects').select()
-
-  if (error) console.log(error)
-
-  return data
-})()
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+
+const projects = ref()
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) console.log(error)
+
+  projects.value = data
+
+  console.log('projects: ', projects.value)
+})()
+</script>
 
 <template>
   <div>
     <h1>Projects Page</h1>
     <RouterLink to="/">Go to home</RouterLink>
+    {{ projects }}
   </div>
 </template>
