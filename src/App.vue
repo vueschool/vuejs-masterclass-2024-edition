f042f0c42084c67cfaaa1b9379aee043a9d7315b
Mostafa Said <mostafasaid1994@gmail.com>
2024-07-02 17:10:08 +0300

    feat: :sparkles: getting started with the layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
-<script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-
-console.log(supabase)
-</script>
+<script setup lang="ts"></script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
+    <form class="w-full max-w-96">
+      <input class="w-full pl-8 bg-background" type="text" placeholder="Search ..." />
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+  </nav>
   <main>
     <RouterView />
   </main>
