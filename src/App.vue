efef3b891d1809303c73e3357db7ba7e6c505954
Mostafa Said <mostafasaid1994@gmail.com>
2024-07-02 22:17:02 +0300

    feat: :sparkles: iconify svg

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,18 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 <template>
   <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
     <form class="w-full max-w-96">
+      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <circle cx="11" cy="11" r="8" />
+          <path d="m21 21l-4.3-4.3" />
+        </g>
+      </svg>
       <Input class="w-full pl-8 bg-background" type="text" placeholder="Search ..." />
     </form>
     <DropdownMenu>
