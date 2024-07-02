020de1d463aba0f871c5e490dfda0bbf0fce3e71
Mostafa Said <mostafasaid1994@gmail.com>
2024-07-02 19:46:48 +0300

    feat: :sparkles: shadcn dropdown and input

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Input from '@/components/ui/input/Input.vue'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+</script>
 
 <template>
   <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
     <form class="w-full max-w-96">
-      <input class="w-full pl-8 bg-background" type="text" placeholder="Search ..." />
+      <Input class="w-full pl-8 bg-background" type="text" placeholder="Search ..." />
     </form>
-    <div class="w-8 h-8 rounded-full bg-white"></div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </nav>
   <main>
     <RouterView />
