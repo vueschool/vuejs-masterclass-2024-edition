c29fb31e89a6d8b4d6d884d53da2b4522b8813e0
Mostafa Said <mostafasaid1994@gmail.com>
2024-07-09 10:46:38 +0300

    fix: :bug: fix App.vue template

--- templates/App.vue
+++ templates/App.vue
@@ -16,13 +16,13 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
     class="flex flex-col h-screen gap-2 border-r fixed bg-muted/40 lg:w-52 w-16 transition-[width]"
   >
     <div class="flex h-16 items-center border-b px-2 lg:px-4 shrink-0 gap-1 justify-between">
-      <Button variant="outline" size="icon" class="w-8 h-8">
+      <button variant="outline" size="icon" class="w-8 h-8">
         <iconify-icon icon="lucide:menu"></iconify-icon>
-      </Button>
+      </button>
 
-      <Button variant="outline" size="icon" class="w-8 h-8">
+      <button variant="outline" size="icon" class="w-8 h-8">
         <iconify-icon icon="lucide:plus"></iconify-icon>
-      </Button>
+      </button>
     </div>
 
     <nav class="flex flex-col gap-2 justify-between h-full relative">
