ba6b066f6de2bfd6c6b70ffd5b56f34319230dc9
Mostafa Said <mostafasaid1994@gmail.com>
2024-08-02 11:53:24 +0300

    feat: :sparkles: suspense with routerview

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
-<script setup lang="ts">
-const MocComponent = defineComponent(async () => {
-  const greeting = ref('Hello')
-
-  await new Promise((resolve) => {
-    greeting.value = 'Hello from the database!'
-
-    setTimeout(() => {
-      resolve(true)
-    }, 1000)
-  })
-
-  return () => h('p', greeting.value)
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <AuthLayout>
-    <Suspense>
-      <MocComponent />
+    <RouterView v-slot="{ Component, route }">
+      <Suspense v-if="Component" :timeout="0">
+        <Component :is="Component" :key="route.name"></Component>
 
-      <template #fallback>
-        <span>Loading ..</span>
-      </template>
-    </Suspense>
-    <!-- <RouterView /> -->
+        <template #fallback>
+          <span>Loading ..</span>
+        </template>
+      </Suspense>
+    </RouterView>
   </AuthLayout>
 </template>
--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -5,13 +5,15 @@ import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
 
 const tasks = ref<Tables<'tasks'>[] | null>(null)
-;(async () => {
+const getTasks = async () => {
   const { data, error } = await supabase.from('tasks').select()
 
   if (error) console.log(error)
 
   tasks.value = data
-})()
+}
+
+await getTasks()
 
 const columns: ColumnDef<Tables<'tasks'>>[] = [
   {
