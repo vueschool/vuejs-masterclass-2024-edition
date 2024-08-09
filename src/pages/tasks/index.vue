166c7debb2dced998e817ed3aebb7e8232a3f4c9
Mostafa Said <mostafasaid1994@gmail.com>
2024-08-09 10:26:09 +0300

    feat: :sparkles: add project links to tasks table

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -8,7 +8,14 @@ usePageStore().pageData.title = 'My Tasks'
 
 const tasks = ref<Tables<'tasks'>[] | null>(null)
 const getTasks = async () => {
-  const { data, error } = await supabase.from('tasks').select()
+  const { data, error } = await supabase.from('tasks').select(`
+    *,
+    projects (
+      id,
+      name,
+      slug
+    )
+  `)
 
   if (error) console.log(error)
 
@@ -47,10 +54,17 @@ const columns: ColumnDef<Tables<'tasks'>>[] = [
     }
   },
   {
-    accessorKey: 'project_id',
+    accessorKey: 'projects',
     header: () => h('div', { class: 'text-left' }, 'Project'),
     cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('project_id'))
+      return h(
+        RouterLink,
+        {
+          to: `/projects/${row.original.projects.slug}`,
+          class: 'text-left font-medium hover:bg-muted block w-full'
+        },
+        () => row.getValue('projects').name
+      )
     }
   },
   {
