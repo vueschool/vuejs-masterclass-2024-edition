caf4d3099e40ddf163815e097bed663ae1e173ef
Mostafa Said <mostafasaid1994@gmail.com>
2024-08-09 12:46:22 +0300

    feat: :sparkles: sortout typescript for complex Supabase Queries

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -3,12 +3,11 @@ import { supabase } from '@/lib/supabaseClient'
 import type { Tables } from '../../../database/types'
 import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
+import type { QueryData } from '@supabase/supabase-js'
 
 usePageStore().pageData.title = 'My Tasks'
 
-const tasks = ref<Tables<'tasks'>[] | null>(null)
-const getTasks = async () => {
-  const { data, error } = await supabase.from('tasks').select(`
+const tasksWithProjectsQuery = supabase.from('tasks').select(`
     *,
     projects (
       id,
@@ -17,6 +16,12 @@ const getTasks = async () => {
     )
   `)
 
+type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
+
+const tasks = ref<TasksWithProjects | null>(null)
+const getTasks = async () => {
+  const { data, error } = await tasksWithProjectsQuery
+
   if (error) console.log(error)
 
   tasks.value = data
@@ -24,7 +29,7 @@ const getTasks = async () => {
 
 await getTasks()
 
-const columns: ColumnDef<Tables<'tasks'>>[] = [
+const columns: ColumnDef<TasksWithProjects[0]>[] = [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
@@ -57,14 +62,16 @@ const columns: ColumnDef<Tables<'tasks'>>[] = [
     accessorKey: 'projects',
     header: () => h('div', { class: 'text-left' }, 'Project'),
     cell: ({ row }) => {
-      return h(
-        RouterLink,
-        {
-          to: `/projects/${row.original.projects.slug}`,
-          class: 'text-left font-medium hover:bg-muted block w-full'
-        },
-        () => row.getValue('projects').name
-      )
+      return row.original.projects
+        ? h(
+            RouterLink,
+            {
+              to: `/projects/${row.original.projects.slug}`,
+              class: 'text-left font-medium hover:bg-muted block w-full'
+            },
+            () => row.original.projects?.name
+          )
+        : ''
     }
   },
   {
