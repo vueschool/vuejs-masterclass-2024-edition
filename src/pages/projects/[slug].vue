4c41ff3314e242d0d088c66e732cfb4e19db3bf1
Mostafa Said <mostafasaid1994@gmail.com>
2024-08-15 20:28:25 +0300

    feat: :sparkles: update project page title

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -6,6 +6,13 @@ const route = useRoute('/projects/[slug]')
 
 const project = ref<Project | null>(null)
 
+watch(
+  () => project.value?.name,
+  () => {
+    usePageStore().pageData.title = `Project: ${project.value?.name || ''}`
+  }
+)
+
 const getProjects = async () => {
   const { data, error } = await projectQuery(route.params.slug)
 
