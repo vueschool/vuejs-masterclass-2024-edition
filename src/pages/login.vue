9e1f2cd7aba8a604875717f5b47f6e7cbb9c4e04
Mostafa Said <mostafasaid1994@gmail.com>
2024-09-13 08:55:50 +0300

    feat: :sparkles: login users

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,4 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+
+const formData = ref({
+  email: '',
+  password: ''
+})
+
+const router = useRouter()
+
+const signin = async () => {
+  const { error } = await supabase.auth.signInWithPassword({
+    email: formData.value.email,
+    password: formData.value.password
+  })
+
+  if (error) return console.log(error)
+
+  router.push('/')
+}
+</script>
 
 <template>
   <div class="mx-auto flex w-full justify-center items-center p-10 text-center -mt-20 min-h-[90vh]">
@@ -13,17 +33,28 @@
           <Separator label="Or" />
         </div>
 
-        <form class="grid gap-4">
+        <form class="grid gap-4" @submit.prevent="signin">
           <div class="grid gap-2">
             <Label id="email" class="text-left">Email</Label>
-            <Input type="email" placeholder="johndoe19@example.com" required />
+            <Input
+              type="email"
+              placeholder="johndoe19@example.com"
+              required
+              v-model="formData.email"
+            />
           </div>
           <div class="grid gap-2">
             <div class="flex items-center">
               <Label id="password">Password</Label>
               <a href="#" class="inline-block ml-auto text-xs underline"> Forgot your password? </a>
             </div>
-            <Input id="password" type="password" autocomplete required />
+            <Input
+              id="password"
+              type="password"
+              autocomplete
+              required
+              v-model="formData.password"
+            />
           </div>
           <Button type="submit" class="w-full"> Login </Button>
         </form>
