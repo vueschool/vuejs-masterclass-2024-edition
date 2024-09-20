dda5989fa6618d02c92414302e736129aa680634
Mostafa Said <mostafasaid1994@gmail.com>
2024-09-20 17:58:09 +0300

    feat: :sparkles: getsession

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import { supabase } from './lib/supabaseClient'
+
 const errorStore = useErrorStore()
+const authStore = useAuthStore()
 
 onErrorCaptured((error) => {
   errorStore.setError({ error })
+})
+
+onMounted(async () => {
+  const { data } = await supabase.auth.getSession()
+  if (data.session?.user) await authStore.setAuth(data.session)
 })
 </script>
 
