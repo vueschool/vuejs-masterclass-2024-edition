a88953279bbf0d13a51787387c3ad0c99ede64fc
Mostafa Said <mostafasaid1994@gmail.com>
2024-10-04 12:03:36 +0300

    update seeders and migrations with primary tetsing user

--- database/seed.js
+++ database/seed.js
@@ -3,7 +3,16 @@
 import { fakerEN_US as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
+const supabase = createClient(
+  process.env.VITE_SUPABASE_URL,
+  process.env.SERVICE_ROLE_KEY
+)
+
+const testingUserEmail = process.env.TESTING_USER_EMAIL
+if (!testingUserEmail) {
+  console.error('Have you forgot to add TESTING_USER_EMAIL to your .env file?')
+  process.exit()
+}
 
 const logErrorAndExit = (tableName, error) => {
   console.error(
@@ -16,7 +25,62 @@ const logStep = (stepMessage) => {
   console.log(stepMessage)
 }
 
-const seedProjects = async (numEntries) => {
+const PrimaryTestUserExists = async () => {
+  logStep('Checking if primary test user exists...')
+  const { data, error } = await supabase
+    .from('auth.users')
+    .select('id, email')
+    .eq('email', testingUserEmail)
+    .single()
+
+  if (error) {
+    console.log('Primary test user not found. Will create one.')
+    return false
+  }
+
+  logStep('Primary test user is found.')
+  return data?.id
+}
+
+const createPrimaryTestUser = async () => {
+  logStep('Creating primary test user...')
+  const firstName = 'Test'
+  const lastName = 'Account'
+  const userName = 'testaccount1'
+  const email = testingUserEmail
+  const { data, error } = await supabase.auth.signUp({
+    email: email,
+    password: 'password',
+    options: {
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        full_name: firstName + ' ' + lastName,
+        username: userName
+      }
+    }
+  })
+
+  if (error) {
+    logErrorAndExit('Users', error)
+  }
+
+  if (data) {
+    const userId = data.user.id
+    await supabase.from('profiles').insert({
+      id: userId,
+      full_name: firstName + ' ' + lastName,
+      username: userName,
+      bio: 'The main testing account',
+      avatar_url: `https://i.pravatar.cc/150?u=${data.user.id}`
+    })
+
+    logStep('Primary test user created successfully.')
+    return userId
+  }
+}
+
+const seedProjects = async (numEntries, userId) => {
   logStep('Seeding projects...')
   const projects = []
 
@@ -28,11 +92,14 @@ const seedProjects = async (numEntries) => {
       slug: name.toLocaleLowerCase().replace(/ /g, '-'),
       description: faker.lorem.paragraphs(2),
       status: faker.helpers.arrayElement(['in-progress', 'completed']),
-      collaborators: faker.helpers.arrayElements([1, 2, 3])
+      collaborators: faker.helpers.arrayElements([userId])
     })
   }
 
-  const { data, error } = await supabase.from('projects').insert(projects).select('id')
+  const { data, error } = await supabase
+    .from('projects')
+    .insert(projects)
+    .select('id')
 
   if (error) return logErrorAndExit('Projects', error)
 
@@ -41,7 +108,7 @@ const seedProjects = async (numEntries) => {
   return data
 }
 
-const seedTasks = async (numEntries, projectsIds) => {
+const seedTasks = async (numEntries, projectsIds, userId) => {
   logStep('Seeding tasks...')
   const tasks = []
 
@@ -51,12 +118,16 @@ const seedTasks = async (numEntries, projectsIds) => {
       status: faker.helpers.arrayElement(['in-progress', 'completed']),
       description: faker.lorem.paragraph(),
       due_date: faker.date.future(),
+      profile_id: userId,
       project_id: faker.helpers.arrayElement(projectsIds),
-      collaborators: faker.helpers.arrayElements([1, 2, 3])
+      collaborators: faker.helpers.arrayElements([userId])
     })
   }
 
-  const { data, error } = await supabase.from('tasks').insert(tasks).select('id')
+  const { data, error } = await supabase
+    .from('tasks')
+    .insert(tasks)
+    .select('id')
 
   if (error) return logErrorAndExit('Tasks', error)
 
@@ -66,8 +137,21 @@ const seedTasks = async (numEntries, projectsIds) => {
 }
 
 const seedDatabase = async (numEntriesPerTable) => {
-  const projectsIds = (await seedProjects(numEntriesPerTable)).map((project) => project.id)
-  await seedTasks(numEntriesPerTable, projectsIds)
+  let userId
+
+  const testUserId = await PrimaryTestUserExists()
+
+  if (!testUserId) {
+    const primaryTestUserId = await createPrimaryTestUser()
+    userId = primaryTestUserId
+  } else {
+    userId = testUserId
+  }
+
+  const projectsIds = (await seedProjects(numEntriesPerTable, userId)).map(
+    (project) => project.id
+  )
+  await seedTasks(numEntriesPerTable, projectsIds, userId)
 }
 
 const numEntriesPerTable = 10
--- supabase/migrations/20240926141009_tasks-schema.sql
+++ supabase/migrations/20240926141009_tasks-schema.sql
@@ -8,6 +8,7 @@ create table
     status current_status default 'in-progress' not null,
     description text not null,
     due_date date default null,
+    profile_id uuid references profiles (id) on delete cascade not null,
     project_id bigint references projects (id) default null,
     collaborators text array default array[]::varchar[] not null
   );
