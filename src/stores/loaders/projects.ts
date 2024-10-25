c6c2dd36227842fde3f4d2178da0785814c47eb6
Mostafa Said <mostafasaid1994@gmail.com>
2024-10-26 00:26:56 +0300

    stale while revalidate projects

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -6,12 +6,28 @@ export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects | null>(null)
   const loadProjects = useMemoize(async (key: string) => await projectsQuery)
 
+  const validateCache = () => {
+    if (projects.value?.length) {
+      projectsQuery.then(({ data }) => {
+        if (JSON.stringify(projects.value) === JSON.stringify(data)) {
+          console.log('Cached and fresh data matched!')
+          return
+        } else {
+          console.log('Somehting has changed!')
+          loadProjects.delete('projects')
+        }
+      })
+    }
+  }
+
   const getProjects = async () => {
     const { data, error, status } = await loadProjects('projects')
 
     if (error) useErrorStore().setError({ error, customCode: status })
 
     projects.value = data
+
+    validateCache()
   }
 
   return {
