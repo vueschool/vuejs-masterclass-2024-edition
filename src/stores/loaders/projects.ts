9912656917cb9d15a3f4de8917c5d9c6a34313c4
Mostafa Said <mostafasaid1994@gmail.com>
2024-10-25 23:56:53 +0300

    useMemoize from vueuse to optimize Pinia loader functionst

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -1,16 +1,33 @@
 import { projectsQuery } from '@/utils/supaQueries'
+import { useMemoize } from '@vueuse/core'
 import type { Projects } from '@/utils/supaQueries'
 
 export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects | null>(null)
-  const getProjects = async () => {
-    if (projects.value?.length) return
+  const loadProjects = useMemoize(async (key: string) => await projectsQuery)
 
-    const { data, error, status } = await projectsQuery
+  const validateCache = () => {
+    if (projects.value?.length) {
+      projectsQuery.then(({ data }) => {
+        if (JSON.stringify(projects.value) === JSON.stringify(data)) {
+          console.log('Cached and fresh data matched!')
+          return
+        } else {
+          console.log('Somehting has changed!')
+          loadProjects.delete('projects')
+        }
+      })
+    }
+  }
+
+  const getProjects = async () => {
+    const { data, error, status } = await loadProjects('projects')
 
     if (error) useErrorStore().setError({ error, customCode: status })
 
     projects.value = data
+
+    validateCache()
   }
 
   return {
