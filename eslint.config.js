a7e47d13c2d54b41d0970b9228fbd86d90f5fb77
Mostafa Said <mostafasaid1994@gmail.com>
2024-10-31 20:28:13 +0300

    eslint 9

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}']
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
+  },
+  ...pluginVue.configs['flat/essential'],
+  {
+    rules: {
+      'vue/multi-word-component-names': 0
+    }
+  },
+  ...vueTsEslintConfig(),
+  skipFormatting
+]
