914c50096366cb020e685cc0ba9b651289b2df7c
Mostafa Said <mostafasaid1994@gmail.com>
2024-11-14 19:54:45 +0200

    update project status

--- src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
+++ src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
@@ -1,14 +1,35 @@
 <script setup lang="ts">
 const value = defineModel<'in-progress' | 'completed'>()
+
+const emit = defineEmits(['commit'])
+
+const toggleValue = () => {
+  value.value = value.value === 'completed' ? 'in-progress' : 'completed'
+  emit('commit')
+}
 </script>
 
 <template>
-  <div class="text-2xl cursor-pointer">
-    <iconify-icon
-      v-if="value === 'completed'"
-      icon="lucide:circle-check"
-      class="text-green-500"
-    />
-    <iconify-icon v-else icon="lucide:circle-dot" class="text-gray-500" />
+  <div class="text-2xl cursor-pointer" @click="toggleValue">
+    <Transition mode="out-in">
+      <iconify-icon
+        v-if="value === 'completed'"
+        icon="lucide:circle-check"
+        class="text-green-500"
+      />
+      <iconify-icon v-else icon="lucide:circle-dot" class="text-gray-500" />
+    </Transition>
   </div>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: transform 0.1s;
+}
+
+.v-enter-from,
+.v-leave-to {
+  transform: scale(0.3);
+}
+</style>
--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -35,7 +35,10 @@ await getProject(slug)
     <TableRow>
       <TableHead> Status </TableHead>
       <TableCell>
-        <AppInPlaceEditStatus v-model="project.status" />
+        <AppInPlaceEditStatus
+          v-model="project.status"
+          @commit="updateProject"
+        />
       </TableCell>
     </TableRow>
     <TableRow>
